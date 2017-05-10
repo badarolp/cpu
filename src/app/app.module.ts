333d346584b2470a8671bff23d14d2b4d4951a1b
badarovca <badaro_sacra@hotmail.com>
2017-05-09 22:55:20 -0300

    [+] Adição de arquivos - src

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { AuthService } from './../providers/auth-service';
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import {Pedidos} from '../pages/pedidos/pedidos';
+
+import { MyApp } from './app.component';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    Pedidos
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    Pedidos
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthService
+  ]
+})
+export class AppModule {}
---
+++ src/pages/login/login.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { NavController, AlertController, LoadingController, Loading, IonicPage } from 'ionic-angular';
+import { AuthService } from '../../providers/auth-service';
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class Login {
+  loading: Loading;
+  registerCredentials = { email: '', password: '' };
+
+  constructor(private nav: NavController, private auth: AuthService, private alertCtrl: AlertController, private loadingCtrl: LoadingController) { }
+
+  public createAccount() {
+    this.nav.push('Register');
+  }
+
+  public login() {
+    this.showLoading()
+    this.auth.login(this.registerCredentials).subscribe(allowed => {
+      if (allowed) {
+        this.nav.setRoot('Home');
+      } else {
+        this.showError("Acesso negado");
+      }
+    },
+      error => {
+        this.showError(error);
+      });
+  }
+
+  showLoading() {
+    this.loading = this.loadingCtrl.create({
+      content: 'Please wait...',
+      dismissOnPageChange: true
+    });
+    this.loading.present();
+  }
+
+  showError(text) {
+    this.loading.dismiss();
+
+    let alert = this.alertCtrl.create({
+      title: 'Fail 2',
+      subTitle: text,
+      buttons: ['OK']
+    });
+    alert.present(prompt);
+  }
+}
