da170183a8fb70c2e09c1a6f15311d36bff520b4
badarovca <badaro_sacra@hotmail.com>
2017-05-10 22:56:45 -0300

    [+] Tela de Cadastro completada

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -10,6 +10,7 @@ import { AuthService } from '../../providers/auth-service';
 export class Register {
   createSuccess = false;
   registerCredentials = { email: '', password: '' };
+  gaming: string = "n64";
 
   constructor(private nav: NavController, private auth: AuthService, private alertCtrl: AlertController) { }
 
